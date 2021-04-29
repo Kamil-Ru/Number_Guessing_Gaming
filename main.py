14c3bf802e1ac3730471165340132a01ce8d4e02
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-29 21:42:17 +0200

    I deleted white lines

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from art import logo
 import random
 from replit import clear
 
-
 # logo, start
 clear()
 print(logo)
@@ -13,7 +12,7 @@ I'm thinking of a number between 1 and 100.''')
 random_number = random.randint(1,100)
 
 # for testing
-print(f"Pssst, the correct answer is {random_number}")
+# print(f"Pssst, the correct answer is {random_number}")
 
 # Choose a difficulty
 while True:
@@ -32,19 +31,15 @@ while True:
     if attempt == 0:
         print("You've run out of guesses, you lose.")
         break
-    
     print(f"You have {attempt} attempts remaining to guess the number.")
     guess = int(input("Make a guess: "))
     
     if guess == random_number:
         print(f"You got it! The answer was {random_number}.")
         break
-        
     elif guess > random_number:
         print("Too high.")
         attempt -= 1
-
     elif guess < random_number:
         print("Too low.")
         attempt -= 1
-
